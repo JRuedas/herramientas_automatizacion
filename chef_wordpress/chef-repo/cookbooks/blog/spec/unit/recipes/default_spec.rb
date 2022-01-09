d8a35ba1b4078bf6e9cb31e239768ae00a59d120
JRuedas <11670560+JRuedas@users.noreply.github.com>
2022-01-09 18:33:18 +0100

    Adds centos test

--- chef_wordpress/chef-repo/cookbooks/blog/spec/unit/recipes/default_spec.rb
+++ chef_wordpress/chef-repo/cookbooks/blog/spec/unit/recipes/default_spec.rb
@@ -1,9 +1,31 @@
 require 'spec_helper'
 
 describe 'blog::default' do
-    platform 'ubuntu'
 
-    context 'LAMP recipes should be included' do
+    context 'LAMP recipes should be included in Ubuntu' do
+
+        platform 'ubuntu'
+
+        it 'should have `apache` recipe included' do
+            expect(chef_run).to include_recipe('blog::apache')
+        end
+
+        it 'should have `php` recipe included' do
+            expect(chef_run).to include_recipe('blog::php')
+        end
+
+        it 'should have `mysql` recipe included' do
+            expect(chef_run).to include_recipe('blog::mysql')
+        end
+
+        it 'should have `wordpress` recipe included' do
+            expect(chef_run).to include_recipe('blog::wordpress')
+        end
+    end
+
+    context 'LAMP recipes should be included in CentOS' do
+
+        platform 'centos'
 
         it 'should have `apache` recipe included' do
             expect(chef_run).to include_recipe('blog::apache')
