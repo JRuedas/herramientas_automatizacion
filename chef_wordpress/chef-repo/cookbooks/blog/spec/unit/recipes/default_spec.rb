dbdf0506f9af10bc2d05d4ef17666bd694155b1b
JRuedas <11670560+JRuedas@users.noreply.github.com>
2022-01-09 21:51:01 +0100

    Adds more tests

--- chef_wordpress/chef-repo/cookbooks/blog/spec/unit/recipes/default_spec.rb
+++ chef_wordpress/chef-repo/cookbooks/blog/spec/unit/recipes/default_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe 'blog::default' do
 
-    context 'LAMP recipes should be included in Ubuntu' do
+    platform 'ubuntu'
 
-        platform 'ubuntu'
+    context 'LAMP recipes should be included Ubuntu' do
 
         it 'should have `apache` recipe included' do
             expect(chef_run).to include_recipe('blog::apache')
@@ -23,7 +23,50 @@ describe 'blog::default' do
         end
     end
 
-    context 'LAMP recipes should be included in CentOS' do
+    context 'Default attributes should be correct Ubuntu' do
+
+        it "should have default document root '/var/www/html'" do
+            expect(chef_run.node['blog']['document_root']).to eq('/var/www/html') 
+        end
+    
+        it "should have default apache port '8080'" do
+            expect(chef_run.node['blog']['apache_port']).to eq('8080') 
+        end
+
+        it "should have default mysql port '3306'" do
+            expect(chef_run.node['blog']['mysql_port']).to eq('3306') 
+        end
+
+        it "should have default mysql version '8.0'" do
+            expect(chef_run.node['blog']['mysql_version']).to eq('8.0') 
+        end
+
+        it "should have default wordpress version '5.7'" do
+            expect(chef_run.node['blog']['wp_version']).to eq('5.7') 
+        end
+
+        it "should have default wordpress owner 'vagrant'" do
+            expect(chef_run.node['blog']['wp_owner']).to eq('vagrant') 
+        end
+
+        it "should have default wordpress host 'localhost'" do
+            expect(chef_run.node['blog']['wp_host']).to eq('localhost') 
+        end
+
+        it "should have default database name 'wordpress'" do
+            expect(chef_run.node['blog']['db_name']).to eq('wordpress') 
+        end
+
+        it "should have default database user 'wpuser'" do
+            expect(chef_run.node['blog']['db_user']).to eq('wpuser') 
+        end
+
+        it "should have default database password 'wppass'" do
+            expect(chef_run.node['blog']['db_pass']).to eq('wppass') 
+        end
+    end
+
+    context 'LAMP recipes should be included CentOS' do
 
         platform 'centos'
 
@@ -41,6 +84,51 @@ describe 'blog::default' do
 
         it 'should have `wordpress` recipe included' do
             expect(chef_run).to include_recipe('blog::wordpress')
+        end
+    end
+
+    context 'Default attributes should be correct Ubuntu' do
+
+        platform 'centos'
+
+        it "should have default document root '/var/www/html'" do
+            expect(chef_run.node['blog']['document_root']).to eq('/var/www/html') 
+        end
+    
+        it "should have default apache port '8080'" do
+            expect(chef_run.node['blog']['apache_port']).to eq('8080') 
+        end
+
+        it "should have default mysql port '3306'" do
+            expect(chef_run.node['blog']['mysql_port']).to eq('3306') 
+        end
+
+        it "should have default mysql version '8.0'" do
+            expect(chef_run.node['blog']['mysql_version']).to eq('8.0') 
+        end
+
+        it "should have default wordpress version '5.7'" do
+            expect(chef_run.node['blog']['wp_version']).to eq('5.7') 
+        end
+
+        it "should have default wordpress owner 'vagrant'" do
+            expect(chef_run.node['blog']['wp_owner']).to eq('vagrant') 
+        end
+
+        it "should have default wordpress host 'localhost'" do
+            expect(chef_run.node['blog']['wp_host']).to eq('localhost') 
+        end
+
+        it "should have default database name 'wordpress'" do
+            expect(chef_run.node['blog']['db_name']).to eq('wordpress') 
+        end
+
+        it "should have default database user 'wpuser'" do
+            expect(chef_run.node['blog']['db_user']).to eq('wpuser') 
+        end
+
+        it "should have default database password 'wppass'" do
+            expect(chef_run.node['blog']['db_pass']).to eq('wppass') 
         end
     end
 end
--- chef_wordpress/chef-repo/cookbooks/blog/test/integration/default/serverspec/default_spec.rb
+++ chef_wordpress/chef-repo/cookbooks/blog/test/integration/default/serverspec/default_spec.rb
@@ -2,12 +2,97 @@
 
 # The Chef InSpec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec/resources/
+
 require 'spec_helper'
 
+########### Apache ###########
+
 describe port(8080) do
-  it { should be_listening }
+    it { should be_listening }
+end
+  
+describe port(80) do
+    it { should_not be_listening }
 end
 
+if os[:family] == 'ubuntu'
+    describe service('apache2') do
+        it { should be_running }
+    end
+
+    describe file('/etc/apache2/sites-available/localhost.conf') do
+        it { should exist }
+        it { should be_file }
+        it { should contain "<VirtualHost *:8080>" }
+        it { should contain "DocumentRoot /var/www/html" }
+        it { should contain "<Directory /var/www/html>" }
+    end
+
+    describe file('/etc/apache2/sites-enabled/localhost.conf') do
+        it { should exist }
+        it { should be_symlink }
+    end
+end
+
+if os[:family] == 'redhat'
+    describe service('httpd') do
+        it { should be_running }
+    end
+
+    describe file('/etc/httpd/sites-available/localhost.conf') do
+        it { should exist }
+        it { should be_file }
+        it { should contain "<VirtualHost *:8080>" }
+        it { should contain "DocumentRoot /var/www/html" }
+        it { should contain "<Directory /var/www/html>" }
+    end
+
+    describe file('/etc/httpd/sites-enabled/localhost.conf') do
+        it { should exist }
+        it { should be_symlink }
+    end
+end
+
+########### PHP ###########
+ 
+if os[:family] == 'ubuntu'
+    describe package('php7.4-mysql') do
+      it { should be_installed }
+    end
+end
+  
+if os[:family] == 'redhat'
+    describe package('php-mysqlnd') do
+        it { should be_installed }
+    end
+
+    describe package('php-json') do
+        it { should be_installed }
+    end
+end
+
+########### MySQL ###########
+
 describe port(3306) do
-  it { should be_listening }
+    it { should be_listening }
+end
+
+describe service('mysql') do
+    it { should be_running }
+end
+
+########### Wordpress ###########
+
+describe file('/usr/bin/wp') do
+    it { should exist }
+    it { should be_file }
+    it { should be_executable }
+end
+
+describe file('/var/www/html/wp-config.php') do
+    it { should exist }
+    it { should be_file }
+    it { should contain "define( 'DB_NAME', 'wordpress' );" }
+    it { should contain "define( 'DB_USER', 'wpuser' );" }
+    it { should contain "define( 'DB_PASSWORD', 'wppass' );" }
 end
